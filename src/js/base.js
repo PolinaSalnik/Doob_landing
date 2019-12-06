3f8a790a6e46b8c3794bfc94afa052b2659f1beb
PolinaSalnik <55506635+PolinaSalnik@users.noreply.github.com>
2019-12-06 15:12:14 +0300

    add

--- src/js/base.js
+++ src/js/base.js
@@ -1,1 +1,16 @@
-console.log('Привет')
+$(document).ready(function(){
+$(".lazy").slick({
+    dots: true,
+    lazyLoad: 'ondemand',
+    infinite: true
+});
+});
+
+$(document).ready(function(){
+$(".menuToggle").click(function () {
+    $('.nav').slideToggle("slow", function () {
+        $(".nav ul").show();
+    });
+});
+});
+
